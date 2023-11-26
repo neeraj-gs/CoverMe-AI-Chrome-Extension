347cc02f7e71e9c118f5cdf0a9912fd69434da30
neeraj-gs <gsneeraj05@gmail.com>
2023-11-26 09:30:58 +0530

    Added basic skeleton of the profiel section apge

---
+++ cover-me/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='flex flex-col mx-5'>
+        {/* Headers section for the profile a */}
+        <div className='flex flex-row justify-between my-4 items-center'>
+            <h2 className='text-2xl font-extrabold'>Profile</h2>
+            <button className='rounded-full border-2 border-b-2 border-r-2 border-black px-4 py-3 text-lg font-bold transition-all hover:translate-y-[2px] hover:bg-slate-200'>Back Button</button>
+        </div>
+        
+
+
+    </div>
+  )
+}
+
+export default Profile
