0e161b4209a24b49fa70b7f61892676812206069
neeraj-gs <gsneeraj05@gmail.com>
2023-11-26 09:17:21 +0530

    Added and updated teh cofig file to include flowbite inour project

--- cover-me/tailwind.config.js
+++ cover-me/tailwind.config.js
@@ -2,9 +2,10 @@ module.exports = {
   content: [
     "./public/**/*.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
