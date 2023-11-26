0649ce7a9bc8d2b5a32cef74d38413167f359008
neeraj-gs <gsneeraj05@gmail.com>
2023-11-26 09:19:14 +0530

    Added flowbite and changed basic ui

--- cover-me/src/components/Generator.jsx
+++ cover-me/src/components/Generator.jsx
@@ -9,7 +9,7 @@ const Generator = () => {
             {/* First ROw for the Header  */}
             <div className='flex flex-row justify-between mx-5 my-5 items-center'>
                 <button className='rounded-xl border-4 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:translate-y-[2px] '>Generate</button>
-                <h2 className='text-xl font-extraboldbold'>CoverMe: Linkedin</h2>
+                <h2 className='text-2xl font-extrabold'>CoverMe: Linkedin</h2>
                 <button className='rounded-xl border-4 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:translate-y-[2px] '>Profile</button>
             </div>
 
@@ -17,7 +17,7 @@ const Generator = () => {
             <div className='flex mx-5'>
                 <textarea
                     rows={12}
-                    className='w-3/4 items-center flex justify-center'
+                    className='w-full'
                     placeholder='Generated Customized Cover Letter'
                 />
             </div>
