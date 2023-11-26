48dcffd3cb037a1a6dfca5ed983858b9df369f1f
neeraj-gs <gsneeraj05@gmail.com>
2023-11-26 10:03:14 +0530

    Started working on profiel section and added code to dave the open ai and resume even on switching need ot add it to local storage to persist the data

--- cover-me/src/App.js
+++ cover-me/src/App.js
@@ -6,16 +6,18 @@ import { ROUTES } from './utils/routes';
 function App() {
 
   const [page,setPage] = useState();
+  const [resume,setResume] = useState();
+  const [openAIKey,setOpenAIKey] = useState();
 
   switch (page){
     case ROUTES.GENERATOR:
-      return <Generator setPage={setPage} />;
+      return <Generator setPage={setPage} resume={resume} setResume={setResume}/>;
 
     case ROUTES.PROFILE:
-      return <Profile setPage={setPage} />;
+      return <Profile setPage={setPage} resume={resume} setResume={setResume} openAIKey={openAIKey} setOpenAIKey={setOpenAIKey} />;
 
       default:
-        return <Generator setPage={setPage} />;
+        return <Generator setPage={setPage} resume={resume} setResume={setResume}/>;
   }
 
 }
--- cover-me/src/components/Profile.jsx
+++ cover-me/src/components/Profile.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import { FaArrowCircleLeft } from "react-icons/fa";
 import { ROUTES } from '../utils/routes';
 
-const Profile = ({setPage}) => {
+const Profile = ({setPage,resume,setResume,openAIKey,setOpenAIKey}) => {
+
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const updatedResume = formData.get("resume");
+        const updatedOpenAIKey = formData.get("openAIKey");
+        setResume(updatedResume)
+        setOpenAIKey(updatedOpenAIKey);
+    }
+
   return (
     <div className='flex flex-col mx-5'>
         {/* Headers section for the profile a */}
@@ -15,7 +25,7 @@ const Profile = ({setPage}) => {
 
 
         {/* form ompoent to submit the resiume adn api key  */}
-        <form className="flex-col">
+        <form className="flex-col" onSubmit={handleSubmit}>
         <div className="mb-6">
           <label
             htmlFor="openAIKey"
@@ -29,6 +39,7 @@ const Profile = ({setPage}) => {
             type="text"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Paste in your api key here"
+            defaultValue={openAIKey}
             required
           />
         </div>
@@ -45,6 +56,7 @@ const Profile = ({setPage}) => {
             rows={8}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Paste in your resume here..."
+            defaultValue={resume}
           ></textarea>
         </div>
         <div className="mb-6 text-center">
