e6f15947a9d5f1408bdae14b20155862463882a9
neeraj-gs <gsneeraj05@gmail.com>
2023-11-27 09:09:19 +0530

    Added a chat completiong code and also imported into the generator componet

--- cover-me/src/components/Generator.jsx
+++ cover-me/src/components/Generator.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { PiUserCirclePlusFill } from "react-icons/pi";
 import { ROUTES } from '../utils/routes';
 import {loadData} from '../utils/localStorage'
+import { postChatGPTMessage } from '../utils/chatGPTUtil';
 
 const Generator = ({setPage,resume,openAIKey}) => {
     const [jobDescription,setJobDescription] = useState("");
@@ -38,7 +39,7 @@ const Generator = ({setPage,resume,openAIKey}) => {
             
             {/* First ROw for the Header  */}
             <div className='flex flex-row justify-between mx-5 my-5 items-center'>
-                <button className='rounded-full border-2 border-b-2 border-r-2 border-black px-4 py-3 text-lg font-bold transition-all hover:translate-y-[2px] hover:bg-slate-200'>Generate</button>
+                <button onClick={generateCoverLetter} className='rounded-full border-2 border-b-2 border-r-2 border-black px-4 py-3 text-lg font-bold transition-all hover:translate-y-[2px] hover:bg-slate-200'>Generate</button>
                 <h2 className='text-2xl font-extrabold underline'>CoverMe: Linkedin</h2>
                 <button onClick={()=> {setPage(ROUTES.PROFILE)}} className='rounded-full border-2 border-b-2 border-r-2 border-black px-2 py-2 text-2xl font-bold transition-all hover:translate-y-[2px] hover:bg-slate-200'>
                     <PiUserCirclePlusFill />
@@ -51,7 +52,7 @@ const Generator = ({setPage,resume,openAIKey}) => {
                     rows={12}
                     className='w-full my-3'
                     placeholder='Generated Customized Cover Letter'
-                    value={jobDescription}
+                    value={coverLetter}
                 />
             </div>
 
--- cover-me/src/utils/chatGPTUtil.js
+++ cover-me/src/utils/chatGPTUtil.js
@@ -16,12 +16,13 @@ export const postChatGPTMessage = async (message, openAIKey) => {
     //req using axios we need to set headers, to get chatgpt end poitn we need a uthorization header
   
     // Create the message object to send to the API
-    const userMessage = { role: "user", content: message };
+    const userMessage = { role: "user", content: message }; //this is format to send req to the chat completion model
+    //system and context are other roles
   
     // Define the data to send in the request body
     const chatGPTData = {
       model: CHATGPT_MODEL,
-      messages: [userMessage],
+      messages: [userMessage], //chatgpt is convo model adn all are like an arrahy of msg and replies
     };
   
     try {
@@ -29,7 +30,8 @@ export const postChatGPTMessage = async (message, openAIKey) => {
       const response = await axios.post(CHATGPT_END_POINT, chatGPTData, config);
   
       // Extract the message content from the API response
-      const message = response?.data?.choices[0]?.message.content;
+      const message = response?.data?.choices[0]?.message.content; //the actual message or cover letter is inside the choices message andntehn  the contnt 
+      //actually reetuens the cover letter for us
   
       // Return the message content
       return message;
