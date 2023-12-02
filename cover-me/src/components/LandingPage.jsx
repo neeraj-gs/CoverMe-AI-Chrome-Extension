eb785c25477847becd095e0d62e0e78a805e3fd8
neeraj-gs <gsneeraj05@gmail.com>
2023-12-02 12:27:18 +0530

    Added vh and cahnged teh height for better visibility

--- cover-me/src/components/LandingPage.jsx
+++ cover-me/src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ import TypeWriterTitle from './TypeWriter'
 
 const LandingPage = ({setPage}) => {
   return (
-    <main className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-slate-100">
+    <main className="bg-gradient-to-r min-h-[60vh] grainy from-rose-100 to-slate-100">
         <nav className='flex items-center justify-between shadow-lg shadow-slate-200'>
             <div className='m-4'>
                 <img src="/icon.png" alt="logo" height={70} width={70} />
@@ -14,7 +14,7 @@ const LandingPage = ({setPage}) => {
                     <PiUserCirclePlusFill />
                 </button>
         </nav>
-        <div className='ml-14 flex flex-col items-center w-3/4 mt-56'>
+        <div className='ml-14 flex flex-col items-center w-3/4 mt-32'>
         <h1 className='text-2xl text-center'>
           Generate Customized Cover Letter From Our  
           <span className='text-green-600 font-bold'> Personal AI</span> and Increased chances of getting shortlisted
